29c2a50b7157e5b9cdc6152a09116f65b8c3384f
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2026-05-14 12:04:37 +0330

    prettier set in eslint & add .editorconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+
+  // --- ADD THIS BLOCK ---
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Prettier runs as an ESLint rule
+    },
+  },
+  prettierConfig,
+  // --- END OF NEW BLOCK ---
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
